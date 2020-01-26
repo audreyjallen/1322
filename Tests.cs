bdbfecedbed49d29a77c4b8df2f77fd1c791bdd5
audreyjallen <60330272+audreyjallen@users.noreply.github.com>
2020-01-26 18:31:48 -0500

    Add files via upload

---
+++ Tests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace AudreyAllen_Assignment1
+{
+    class Tests
+    {
+        private string firstName;
+        private string lastName;
+        private double[] testScores;
+        public Tests(string newFirst, string newLast, int [] newScores)
+        {
+            firstName = newFirst;
+            lastName = newLast;
+            testScores = new double [5];
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                testScores[i] = newScores[i];
+            }
+        }
+        public void setFirst (string newFirst)
+        {
+            firstName = newFirst;
+        }
+        public string getFirst ()
+        {
+            return firstName;
+        }
+        public void setLast (string newLast)
+        {
+            lastName = newLast;
+        }
+        public string getLast ()
+        {
+            return lastName;
+        }
+        public void setScores (double [] newScores)
+        {
+            testScores = newScores;
+        }
+        public double [] getScores ()
+        {
+            return testScores;
+        }
+        public double getAverage ()
+        {
+            double average = 0.0;
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                average = average + testScores[i];
+            }
+            average = average / testScores.Length;
+            return average;
+        }
+        public char getLetter ()
+        {
+            double condition = (int)getAverage() / 10;
+            char letterGrade;
+            switch (condition)
+            {
+                case 10:
+                    letterGrade = 'A';
+                    return letterGrade;
+                case 9:
+                    letterGrade = 'A';
+                    return letterGrade;
+                case 8:
+                    letterGrade = 'B';
+                    return letterGrade;
+                case 7:
+                    letterGrade = 'C';
+                    return letterGrade;
+                case 6:
+                    letterGrade = 'D';
+                    return letterGrade;
+                default:
+                    letterGrade = 'F';
+                    return letterGrade;
+            }
+        }
+        public string toString ()
+        {
+            return string.Format("{0, -13} {1, -13} {2, -8} {3, -8} {4, -8} {5, -8} {6, -8} {7, -12} {8, -7}", firstName, lastName, testScores[0], testScores[1], testScores[2], testScores[3], testScores[4], getAverage().ToString("0.00"), getLetter());
+        }
+    }
+}
+
---
+++ testTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudreyAllen_Assignment1
+{
+    class testTests
+    {
+        public static void Main (string[] args)
+        {
+            string firstName;
+            string lastName;
+            int count = 0;
+            double classAverage = 0.0;
+            int[] testScores = new int [5];
+            Tests[] Tests = new Tests[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter a student's first name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Enter the student's last name: ");
+                lastName = Console.ReadLine();
+                for (int j = 0; j < testScores.Length; j++)
+                {
+                    Console.Write("Enter " + firstName + " " + lastName + "'s score for test " + (j+1) + ": ");
+                    testScores[j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Tests[count] = new Tests(firstName, lastName, testScores);
+                count++;
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine(String.Format("{0, -13} {1, -13} {2, -8} {3, -8} {4, -8} {5, -8} {6, -8} {7, -12} {8, -7}", "First Name: ", "Last Name: ", "Test 1: ", "Test 2: ","Test 3: ", "Test 4: ", "Test 5: ", "Average: ", "Grade:"));
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Tests[i].toString());
+                classAverage = classAverage + Tests[i].getAverage();
+            }
+            classAverage = classAverage / 10;
+            Console.WriteLine();
+            Console.WriteLine("Class average: " + classAverage);
+        }
+    }
+}
